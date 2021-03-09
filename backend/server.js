0aed3d4054164ec5edacfe38f89da61ffb9321bc
Swarup Ranjan Mondal <swaranmon@gmail.com>
2021-03-09 11:50:23 +0530

    serving the product images from backend

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,7 @@ app.get("/api/config/paypal", (req, res) =>
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 app.use(notFound);
 app.use(errorHandler);
